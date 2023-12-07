ab67648de22252a11fd75980b7e1a0a2b4a99318
SIFAQES <siphaxzer@gmail.com>
2023-12-07 20:50:57 +0100

    set interface login

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
-import './bootstrap';
+import * as alpine from 'alpinejs/dist/cdn';
+
+
+export { alpine };
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,27 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+$controller_path = 'App\Http\Controllers';
+
+// Main Page Route
+
+// pages
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+$controller_path = 'App\Http\Controllers';
+
+    Route::get('/', $controller_path . '\pages\HomePage@index')->name('pages-home');
+    Route::get('/page-2', $controller_path . '\pages\Page2@index')->name('pages-page-2');
+
+    
 });
